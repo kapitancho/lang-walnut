b5f798baae186a27ecaca138c4d6a7ec9d4b2526
Marian Kostadinov <manchokapitancho@gmail.com>
2025-03-19 21:42:37 +0100

    Added Open->with

--- src/Implementation/Type/Helper/BaseType.php
+++ src/Implementation/Type/Helper/BaseType.php
@@ -7,17 +7,13 @@ use Walnut\Lang\Blueprint\Type\ProxyNamedType;
 use Walnut\Lang\Blueprint\Type\Type;
 
 trait BaseType {
-	public function toBaseType(Type $targetType, $ignoreSubsetType = false): Type {
+	public function toBaseType(Type $targetType): Type {
 		$step = true;
 		while ($step) {
 			$step = false;
 			if ($targetType instanceof AliasType) {
 				$step = true;
 				$targetType = $targetType->aliasedType;
-			}
-			if (!$ignoreSubsetType && $targetType instanceof SubsetType) {
-				$step = true;
-				$targetType = $targetType->valueType;
 			}
 			if ($targetType instanceof ProxyNamedType) {
 				$step = true;
