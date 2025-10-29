f2dd3c7d3648f7129a5fc658f50ee8fd41f6b21f
Marian Kostadinov <manchokapitancho@gmail.com>
2025-10-29 12:46:28 +0100

    try to fix autoload

---
+++ bin/inc/autoload.php
@@ -0,0 +1,6 @@
+<?php
+if (str_ends_with(__DIR__, 'vendor/walnut/lang/bin/inc')) {
+    require_once __DIR__ . '/../../../../vendor/autoload.php';
+} else {
+    require_once __DIR__ . '/../../vendor/autoload.php';
+}
