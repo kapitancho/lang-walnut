79b47f283fd8ea743b9e8dbd2f83e80ea54f8b0a
Marian Kostadinov <manchokapitancho@gmail.com>
2025-12-06 19:34:55 +0100

    Fix ranges for Integer->clamp

--- src/Implementation/Code/NativeCode/Analyser/Numeric/Clamp.php
+++ src/Implementation/Code/NativeCode/Analyser/Numeric/Clamp.php
@@ -6,6 +6,7 @@ use BcMath\Number;
 use Walnut\Lang\Blueprint\Code\Analyser\AnalyserException;
 use Walnut\Lang\Blueprint\Code\Execution\ExecutionException;
 use Walnut\Lang\Blueprint\Common\Identifier\TypeNameIdentifier;
+use Walnut\Lang\Blueprint\Common\Range\InvalidNumberInterval;
 use Walnut\Lang\Blueprint\Common\Range\MinusInfinity;
 use Walnut\Lang\Blueprint\Common\Range\NumberIntervalEndpoint as NumberIntervalEndpointInterface;
 use Walnut\Lang\Blueprint\Common\Range\PlusInfinity;
@@ -24,13 +25,35 @@ use Walnut\Lang\Implementation\Common\Range\NumberInterval;
 use Walnut\Lang\Implementation\Common\Range\NumberIntervalEndpoint;
 use Walnut\Lang\Implementation\Type\Helper\BaseType;
 use Walnut\Lang\Implementation\Value\IntegerValue;
+use Walnut\Lang\NativeCode\Array\Min;
 
 trait Clamp {
 	use BaseType;
 
+	private function isBefore(
+		MinusInfinity|PlusInfinity|NumberIntervalEndpointInterface $first,
+		MinusInfinity|PlusInfinity|NumberIntervalEndpointInterface $second,
+	): bool {
+		if ($first instanceof MinusInfinity || $second instanceof PlusInfinity) {
+			return true;
+		}
+		if ($first instanceof PlusInfinity || $second instanceof MinusInfinity) {
+			return false;
+		}
+		return $first->value <= $second->value;
+	}
+
+	private function isBetween(
+		MinusInfinity|PlusInfinity|NumberIntervalEndpointInterface $value,
+		MinusInfinity|PlusInfinity|NumberIntervalEndpointInterface $from,
+		MinusInfinity|PlusInfinity|NumberIntervalEndpointInterface $to,
+	): bool {
+		return $this->isBefore($from, $value) && $this->isBefore($value, $to);
+	}
+
 	private function analyseHelper(
 		TypeRegistry $typeRegistry,
-		Type $targetType,
+		IntegerType|RealType $targetType,
 		Type $parameterType,
 	): Type {
 		$parameterType = $this->toBaseType($parameterType);
@@ -41,10 +64,43 @@ trait Clamp {
 			])
 		)) {
 			$minType = $parameterType->types['min'] ?? null;
+			if ($minType !== null) {
+				$minType = $this->toBaseType($minType);
+			}
+			$maxType = $parameterType->types['max'] ?? null;
+			if ($maxType !== null) {
+				$maxType = $this->toBaseType($maxType);
+			}
+
+			$minFrom = $minType === null || $minType instanceof OptionalKeyType ? MinusInfinity::value : $minType->numberRange->min;
+			$numFrom = $targetType->numberRange->min;
+			$maxFrom = match(true) {
+				$maxType === null => $numFrom,
+				$maxType instanceof OptionalKeyType => $this->toBaseType($maxType->valueType)->numberRange->min,
+				default => $maxType->numberRange->min
+			};
+
+			$maxTo = $maxType === null || $maxType instanceof OptionalKeyType ? PlusInfinity::value : $maxType->numberRange->max;
+			$numTo = $targetType->numberRange->max;
+			$minTo = match(true) {
+				$minType === null => $numTo,
+				$minType instanceof OptionalKeyType => $this->toBaseType($minType->valueType)->numberRange->max,
+				default => $minType->numberRange->max
+			};
+
+			$from = match(true) {
+				$this->isBetween($numFrom, $minFrom, $maxFrom) => $numFrom,
+				$this->isBetween($maxFrom, $minFrom, $numFrom) => $maxFrom,
+				default => $minFrom
+			};
+			$to = match(true) {
+				$this->isBetween($numTo, $minTo, $maxTo) => $numTo,
+				$this->isBetween($minTo, $numTo, $maxTo) => $minTo,
+				default => $maxTo
+			};
 			if ($minType instanceof OptionalKeyType) {
 				$minType = $this->toBaseType($minType->valueType);
 			}
-			$maxType = $parameterType->types['max'] ?? null;
 			if ($maxType instanceof OptionalKeyType) {
 				$maxType = $this->toBaseType($maxType->valueType);
 			}
@@ -61,21 +117,22 @@ trait Clamp {
 					$mayHaveError = true;
 				}
 			}
+
 			$isIntRange = $minType instanceof IntegerType && $maxType instanceof IntegerType;
+			$errorType = $typeRegistry->data(
+				$isIntRange ?
+					new TypeNameIdentifier('InvalidIntegerRange') :
+					new TypeNameIdentifier('InvalidRealRange')
+			);
+			try {
+				$interval = new NumberInterval($from, $to);
+			} catch (InvalidNumberInterval) {
+				return $typeRegistry->result($typeRegistry->nothing, $errorType);
+			}
+
 			$isInteger = $targetType instanceof IntegerType && $isIntRange;
-			$min = $minType->numberRange->min === MinusInfinity::value || $maxType->numberRange->min === MinusInfinity::value ?
-				MinusInfinity::value :
-				new Number(min($minType->numberRange->min->value, $maxType->numberRange->min->value));
-			$max = $minType->numberRange->max === PlusInfinity::value || $maxType->numberRange->max === PlusInfinity::value ?
-				PlusInfinity::value :
-				new Number(max($minType->numberRange->max->value, $maxType->numberRange->max->value));
-			$numericType = $isInteger ? $typeRegistry->integer($min, $max) : $typeRegistry->real($min, $max);
-			return $mayHaveError ? $typeRegistry->result($numericType,
-				$typeRegistry->data(
-					$isIntRange ?
-						new TypeNameIdentifier('InvalidIntegerRange') :
-						new TypeNameIdentifier('InvalidRealRange')
-				)
+			$numericType = $isInteger ? $typeRegistry->integerFull($interval) : $typeRegistry->realFull($interval);
+			return $mayHaveError ? $typeRegistry->result($numericType, $errorType
 			) : $numericType;
 		}
 		throw new AnalyserException(sprintf("[%s] Invalid parameter type: %s", __CLASS__, $parameterType));
--- tests/Walnut/Lang/NativeCode/Real/ClampTest.php
+++ tests/Walnut/Lang/NativeCode/Real/ClampTest.php
@@ -2,6 +2,7 @@
 
 namespace Walnut\Lang\Test\NativeCode\Real;
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use Walnut\Lang\Test\CodeExecutionTestHelper;
 
 final class ClampTest extends CodeExecutionTestHelper {
@@ -68,6 +69,56 @@ final class ClampTest extends CodeExecutionTestHelper {
 		);
 		$this->assertEquals("10.7", $result);
 	}
+
+	#[DataProvider('clampReturnRangesProvider')]
+	public function testClampReturnRanges(string $param, string $range, string $returnType, string $expected): void {
+		$result = $this->executeCodeSnippet(
+			"clampFn$param;",
+			valueDeclarations: "
+				getReal = ^ => Real<11.1..11.4> :: 11.2;
+				clampFn = ^range: $range => $returnType :: getReal()->clamp(range);
+			"
+		);
+		$this->assertEquals($expected, $result);
+	}
+
+	public static function clampReturnRangesProvider(): iterable {
+		yield ['[min: 10.3]', '[min: Real<1.5..20>]', 'Real<11.1..20>', '11.2'];
+		yield ['[min: 13.2]', '[min: Real<1.5..20>]', 'Real<11.1..20>', '13.2'];
+		yield ['[:]', '[:]', 'Real<11.1..11.4>', '11.2'];
+		yield ['[:]', '[min: ?Real<1.5..20>]', 'Real<11.1..20>', '11.2'];
+		yield ['[min: 10.3]', '[min: ?Real<1.5..20>]', 'Real<11.1..20>', '11.2'];
+		yield ['[min: 13.2]', '[min: ?Real<1.5..20>]', 'Real<11.1..20>', '13.2'];
+
+		yield ['[min: 13.2]', '[min: Real<12.5..20>]', 'Real<12.5..20>', '13.2']; // <11.1..20>
+		yield ['[:]', '[min: ?Real<12.5..20>]', 'Real<11.1..20>', '11.2'];
+		yield ['[min: 13.2]', '[min: ?Real<12.5..20>]', 'Real<11.1..20>', '13.2'];
+
+		yield ['[min: 10.3, max: 43.2]', '[min: Real<1.5..20>, max: Real<33.3..99.9>]', 'Real<11.1..20>', '11.2']; // <1.5..99.9>
+		yield ['[min: 10.3]', '[min: Real<1.5..20>, max: ?Real<33.3..99.9>]', 'Real<11.1..20>', '11.2'];
+		yield ['[min: 10.3, max: 43.2]', '[min: Real<1.5..20>, max: ?Real<33.3..99.9>]', 'Real<11.1..20>', '11.2'];
+
+		yield ['[min: 10.3, max: 10.2]', '[min: Real<1.5..20>, max: Real<9.99..99.9>]', 'Result<Real<9.99..20>, InvalidRealRange>', '@InvalidRealRange![min: 10.3, max: 10.2]']; // <1.5..99.9>
+		yield ['[min: 10.3, max: 10.4]', '[min: Real<1.5..20>, max: Real<9.99..99.9>]', 'Result<Real<9.99..20>, InvalidRealRange>', '10.4']; // <1.5..99.9>
+		yield ['[min: 10.3, max: 43.2]', '[min: Real<1.5..20>, max: Real<9.99..99.9>]', 'Result<Real<9.99..20>, InvalidRealRange>', '11.2']; // <1.5..99.9>
+		yield ['[min: 13.2, max: 43.2]', '[min: Real<1.5..20>, max: Real<9.99..99.9>]', 'Result<Real<9.99..20>, InvalidRealRange>', '13.2']; // <1.5..99.9>
+		yield ['[min: 10.3]', '[min: Real<1.5..20>, max: ?Real<9.99..99.9>]', 'Result<Real<9.99..20>, InvalidRealRange>', '11.2'];
+		yield ['[min: 10.3, max: 10.2]', '[min: Real<1.5..20>, max: ?Real<9.99..99.9>]', 'Result<Real<9.99..20>, InvalidRealRange>', '@InvalidRealRange![min: 10.3, max: 10.2]'];
+		yield ['[min: 10.3, max: 10.4]', '[min: Real<1.5..20>, max: ?Real<9.99..99.9>]', 'Result<Real<9.99..20>, InvalidRealRange>', '10.4'];
+		yield ['[min: 10.3, max: 43.2]', '[min: Real<1.5..20>, max: ?Real<9.99..99.9>]', 'Result<Real<9.99..20>, InvalidRealRange>', '11.2'];
+		yield ['[min: 13.2, max: 43.2]', '[min: Real<1.5..20>, max: ?Real<9.99..99.9>]', 'Result<Real<9.99..20>, InvalidRealRange>', '13.2'];
+
+		yield ['[min: 10.3, max: 10.2]', '[min: Real<1.5..20>, max: Real<9.99..10.9>]', 'Result<Real<9.99..10.9>, InvalidRealRange>', '@InvalidRealRange![min: 10.3, max: 10.2]']; // <1.5..11.4>>
+		yield ['[min: 10.3, max: 10.4]', '[min: Real<1.5..20>, max: Real<9.99..10.9>]', 'Result<Real<9.99..10.9>, InvalidRealRange>', '10.4']; // <1.5..11.4>
+		yield ['[min: 10.3]', '[min: Real<1.5..20>, max: ?Real<9.99..10.9>]', 'Result<Real<9.99..20>, InvalidRealRange>', '11.2'];
+		yield ['[min: 10.3, max: 10.2]', '[min: Real<1.5..20>, max: ?Real<9.99..10.9>]', 'Result<Real<9.99..20>, InvalidRealRange>', '@InvalidRealRange![min: 10.3, max: 10.2]'];
+		yield ['[min: 10.3, max: 10.4]', '[min: Real<1.5..20>, max: ?Real<9.99..10.9>]', 'Result<Real<9.99..20>, InvalidRealRange>', '10.4'];
+
+		yield ['[min: 4.2, max: 20.4]', '[min: Real<1.5..6>, max: Real<19.99..20.9>]', 'Real<11.1..11.4>', '11.2'];
+
+		yield ['[min: 18.3, max: 10.2]', '[min: Real<17.2..20>, max: Real<9.99..10.9>]', 'Error<InvalidRealRange>', '@InvalidRealRange![min: 18.3, max: 10.2]']; // <1.5..11.4>>
+	}
+
 
 	public function testClampReturnTypeResultReal(): void {
 		$result = $this->executeCodeSnippet(
