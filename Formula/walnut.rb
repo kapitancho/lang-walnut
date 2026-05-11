a1ff7c6dc3a1feca4cb29275eb4bde46024581fb
GitHub Action <action@github.com>
2026-05-11 16:56:43 +0000

    chore: update Homebrew formula for release refs/tags/v0.3.10

--- Formula/walnut.rb
+++ Formula/walnut.rb
@@ -5,10 +5,10 @@ class Walnut < Formula
   desc "Strongly-typed, interpreted programming language with compile-time type checking"
   homepage "https://github.com/walnut-lang/walnut"
   license "MIT"
-  version "0.3.9"
+  version "0.3.10"
 
   url "https://github.com/walnut-lang/walnut/releases/download/v#{version}/walnut.phar"
-  sha256 "752b54123a1cad09dc6e88aedab3fc9636d4b6a7e9688634c67065fa8e56b45f"
+  sha256 "e50802a915683cb00ecccd4f714130f5d41b495370c8498f822a9356f151f3a9"
 
   depends_on "php" => :runtime
 
